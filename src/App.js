959c08862988830338cbda73a25edfd1ac751a8c
juan <juanayala@gmail.com>
2022-10-22 15:53:49 -0300

    add html structure with notificationBox

--- src/App.js
+++ src/App.js
@@ -11,12 +11,25 @@ import avatarRizky from "./images/avatar-rizky-hasanuddin.webp"
 import imageChess from "./images/image-chess.webp"
 
 function App() {
-
+  const actions = {
+    reacted: "Reacted to your recent post",
+    followed: "followed you",
+    joined: "has joined your group",
+    message: "sent you a private message",
+    commented: "commented on your picture",
+    leftGroup: "left the group"
+  }
   return (
     <div className="App">
       <Header />
       <main>
-        <NotificationBox picture={avatarAngela} name="b" action="c" place="d" time="e" />
+        <NotificationBox picture={avatarMark} name="Mark Webber" action={actions.reacted} place="My first tournament today!" time="1m" />
+        <NotificationBox picture={avatarAngela} name="Angela Gray" action={actions.followed} place="" time="5m" />
+        <NotificationBox picture={avatarJacob} name="Jacob Thompson" action={actions.joined} place="Chess Club" time="1 day" />
+        <NotificationBox picture={avatarRizky} name="Rizky Hasanuddin" action={actions.message} place="" time="5 days" />
+        <NotificationBox picture={avatarKimberly} name="Kimberly Smith" action={actions.commented} place={imageChess} time="1 week" />
+        <NotificationBox picture={avatarNathan} name="Nathan Peterson" action={actions.reacted} place="5 end-game strategies to increase your win rate" time="2 weeks" />
+        <NotificationBox picture={avatarAnna} name="Anna Kim" action={actions.leftGroup} place="Chess Club" time="2 weeks" />
       </main>
     </div>
   );
